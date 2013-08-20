e49858a5c791ec77256ae92f10ca0fb665792c6e
tnzk <tnzk.marge@gmail.com>
2013-08-21 00:41:51 +0900

    Read a script via stdin as json

--- onstage.cpp
+++ onstage.cpp
@@ -1,10 +1,18 @@
 #include "stage_command.hpp"
 #include "thestage.hpp"
+#include "picojson.h"
 #include <iostream>
+#include <string>
 #include <cinttypes>
 
 int main()
 {
+
+  picojson::value json;
+  std::cin >> json;
+  picojson::object& obj = json.get<picojson::object>();
+  picojson::array& commands = obj["commands"].get<picojson::array>();
+  std::cout << commands[2] << std::endl;
 
   MoveStageCommand moving;
   IStageCommand& command = moving;
