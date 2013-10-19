f17b4e59a337ef9c9d426d94613ef2fa30763f0d
tnzk <tnzk.marge@gmail.com>
2013-10-19 23:27:48 +0900

    Items build the path of the svg by its name

--- item.cpp
+++ item.cpp
@@ -1,4 +1,6 @@
 #include "item.hpp"
+#include <sstream>
+#include <iostream>
 
 Item::Item(std::string name)
 {
@@ -13,14 +15,11 @@ Item::~Item()
 
 bool Item::Render(cairo_t* cairo)
 {
-  cairo_select_font_face(cairo, "serif", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_BOLD);
-  cairo_set_font_size(cairo, 32.0);
-  cairo_set_source_rgb(cairo, 0.0, 0.0, 1.0);
-  cairo_move_to(cairo, (float)30, 100.0);
-  cairo_show_text(cairo, this->name.c_str());
+  std::stringstream ss;
+  ss << "svgs/" << this->name;
 
   rsvg_init();
-  rsvg = rsvg_handle_new_from_file("svgs/bg-s02-b1-momoroom.svg", NULL);
+  rsvg = rsvg_handle_new_from_file(ss.str().c_str(), NULL);
   rsvg_handle_render_cairo(this->rsvg, cairo);
   rsvg_handle_free(this->rsvg);
   rsvg_term();
